ce9ba8df08b63fcccd9f1b61ef4afc90917713df
Kai <2755168704@qq.com>
2024-10-12 17:29:25 +0800

    学习资料

---
+++ src/main/java/org/example/internetstudy/InternetStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.internetstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetStudyApplication.class, args);
+	}
+
+}
